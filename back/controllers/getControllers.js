d9dc2645e48089cfa6787217ed5cdc75eb5d9c95
pandol <pandol69.kim@gmail.com>
2026-01-29 18:25:35 +0900

    qry 수정

--- back/controllers/getControllers.js
+++ back/controllers/getControllers.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 exports.getTasks =  async (request, response) => {
   const userId = request.params.userId;
   //console.log(userId)
-  try { //WHERE userId = $1  , [userId]
-    const result = await database.pool.query("SELECT * FROM tasks  ORDER BY created_at DESC")
+  try {
+    const result = await database.pool.query("SELECT * FROM tasks WHERE _id = $1  ORDER BY created_at DESC", [userId])
     return response.status(200).json(result.rows)
   } catch (error) {
     return response.status(500).json({message: `Get tasks Error: ${error}`})
